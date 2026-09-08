81793a79dcf28a426c1742c6aed0672e88b41cc6
Joe2MOG <albertjoseph08066227697@gmail.com>
2026-09-08 20:48:29 +0100

    Fix file permissions, strip Windows line endings, add console quit/EOF handling

--- console.py
+++ console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Command interpreter for the AirBnB project."""
-
 import cmd
 
 
@@ -8,6 +7,19 @@ class HBNBCommand(cmd.Cmd):
     """Simple command interpreter."""
 
     prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty input line."""
+        pass
 
 
 if __name__ == "__main__":
